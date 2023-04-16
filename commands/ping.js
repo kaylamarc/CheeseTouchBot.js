43d25948376890adb9d75caf3c40a8d9025da051
kaylamarc <kkcoons@aol.com>
2023-04-16 19:41:07 -0400

    ping command

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+// User uses '/ping' command, bot replies with "Pong!"
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
