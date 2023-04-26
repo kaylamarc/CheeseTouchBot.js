7728f3e9483de096d36e85b91be266c5e8ccaeba
kaylamarc <kkcoons@aol.com>
2023-04-26 13:27:01 -0400

    check if user has cheese touch already

--- events/cheeseTouch.js
+++ events/cheeseTouch.js
@@ -1,4 +1,9 @@
 const { Events } = require('discord.js');
+const fs = require('fs');
+
+// Initialize blacklist with 100 most common English words
+const commonWords = fs.readFileSync('blacklist.txt', 'utf8').split('\n');
+const blacklist = new Set(commonWords.slice(0, 100));
 
 module.exports = {
 	name: Events.MessageCreate,
@@ -9,10 +14,18 @@ module.exports = {
 
         // check if the message contains the codeword
         if (messageContent.includes("test")) {
-            console.log("message contained codeword")
 
-            // DM the user that said codeword
-            message.author.send('YOU HAVE CONTRACTED THE **CHEESE TOUCH**')
+            // get author as guild member
+            const member = message.member;
+
+            // check if they already have the cheese touch
+            if (member.roles.cache.some(role => role.name === 'Cheese Touch')) {
+                console.log('user already has cheese touch');
+                return;
+            }
+
+            // // DM the user that said codeword
+            message.author.send('YOU HAVE CONTRACTED THE **CHEESE TOUCH**\n Please enter a ***new*** codeword that is not already on the blacklist:\n');
 
         }
 	},
